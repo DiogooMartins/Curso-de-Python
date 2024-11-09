68856824fa014c7afa90e8745cd34fea465c1d7f
DIOGO MARTINS <diogomartins2k2@gmail.com>
2024-11-08 21:49:15 -0300

    Delete ex017.py

--- ex017.py
+++
@@ -1,5 +0,0 @@
-from math import hypot
-catop = float(input("Qual o comprimento do cateto oposto? "))
-catad = float(input("Qual o comprimento do cateto adjacnte? "))
-#hi = (cat1*cat1 + cat2*cat2)
-print(" A hipotenusa vai medir {:.2f}".format(hypot(catop, catad)))
