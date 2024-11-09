b8ec37a3d9f81d0a2e312db6fe58a1fe818ffc93
DIOGO MARTINS <diogomartins2k2@gmail.com>
2024-11-08 21:48:43 -0300

    Delete ex013.py

--- ex013.py
+++
@@ -1,4 +0,0 @@
-salario = float(input("Qual é o salário do funcionário? "))
-porcentagem = float(input("Qual a porcentagem de aumento? "))
-novosal = salario + (salario * porcentagem / 100)
-print("Um funcionário que ganhava R${:.2f}, com {:.0f}% de aumento, passa a recber R${:.2f}" .format(salario, porcentagem, novosal))
