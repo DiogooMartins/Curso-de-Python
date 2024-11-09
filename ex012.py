5304ca48d9716fd672b76f531a83e0fc35fe22c5
DIOGO MARTINS <diogomartins2k2@gmail.com>
2024-11-08 21:48:20 -0300

    Delete ex012.py

--- ex012.py
+++
@@ -1,4 +0,0 @@
-valor = float(input('Qual o valor do produto? '))
-desconto = float(input('Qual a porcentagem de desconto? '))
-porcentagem = (desconto * valor)/100
-print("o produto que custava R${}, na promoção com desconto de {:.0f}% vai custar R${:.2f}" .format(valor, desconto, valor - porcentagem))
