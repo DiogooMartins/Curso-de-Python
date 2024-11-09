b8823ffc932927bb7d2af424f130082f4eb3632b
DIOGO MARTINS <diogomartins2k2@gmail.com>
2024-11-08 21:47:24 -0300

    Delete ex006.py

--- ex006.py
+++
@@ -1,6 +0,0 @@
-n = int(input('Digite um número:'))
-#dobro = n*2
-#triplo = n*3
-#raiz = n**(1/2)
-#print('Analisando o número {} \nO seu dobro é: {} \nO seu Triplo é: {} \nSua raiz quadrada é: {}'.format(n, dobro, triplo, raiz))
-print('Seu dobro é: {} \nSeu triplo é: {} \nA raiz quadrada é: {:.2f}'.format(n*2, n*3, n**(1/2)))
