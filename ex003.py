5afa426efff5c74793db0abd011a19f11d9ae362
DIOGO MARTINS <diogomartins2k2@gmail.com>
2024-11-08 21:46:28 -0300

    Delete ex003.py

--- ex003.py
+++
@@ -1,4 +0,0 @@
-numero1 = float (input ('Primeiro número:'))
-numero2 = float (input ('Segundo número:'))
-soma = numero1 + numero2
-print('A soma entre {} e {} vale: {}'.format(numero1, numero2, soma))
