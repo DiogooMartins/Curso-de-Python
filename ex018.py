33e2d23a4686624e82f32ad745eb1249d9d2b203
DIOGO MARTINS <diogomartins2k2@gmail.com>
2024-11-08 21:49:20 -0300

    Delete ex018.py

--- ex018.py
+++
@@ -1,9 +0,0 @@
-#from math import radians, sin, cos, tan
-import math
-ang = float(input("Digite um angulo: "))
-sin = math.sin(math.radians(ang))
-print("O angulo de {} tem o Seno {:.2f}" .format(ang, sin))
-cos = math.cos(math.radians(ang))
-print("O angulo de {} tem o Cosseno {:.2f}" .format(ang, cos))
-tan = math.tan(math.radians(ang))
-print("O angulo de {} tem a Tangente {:.2f}" .format(ang, tan))
